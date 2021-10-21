2adff11e8b57044000102b99654faa6229edbdec
Maria del mar Hernandez <mareshernandez1@gmail.com>
2021-10-20 22:43:41 -0500

    se hicieron hover con manejador de estado

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { PinLayout, HeaderLayout } from './components/main';
+
+const App = () => {
+  return (
+    <div>
+      <HeaderLayout/>
+      <PinLayout />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/main.js
@@ -0,0 +1,4 @@
+import PinLayout from './Pinterest-Layout/Pin-Layout/PinLayout';
+import HeaderLayout from './Header-layout/HeaderLayout';
+
+export { PinLayout, HeaderLayout };
